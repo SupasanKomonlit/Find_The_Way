9dcf62fc74efbd4d1dd10d3dcd2de6d50e7918f9
SupasanKomonlit <supasan_224@hotmail.com>
2017-12-18 07:44:39 +0700

    Have Interface

--- core/src/com/findthewaygame/mygdx/game/MainGame.java
+++ core/src/com/findthewaygame/mygdx/game/MainGame.java
@@ -8,27 +8,36 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MainGame extends ApplicationAdapter {
 	public SpriteBatch MainBatch;
-	public String StateGame = "GameRunning";
-	public int NumMap = 1;
+	public String StateGame;
+	public int NumMap;
 	public int width;
 	public int height;
 	private GameRunning ManageGameRunning;
+	private MainInterface ManageInterface;
 
 //for testing int
 //	private int Result = 26/25;
 	
 // About Map	
-	public int SizeBlock = 25;
+	public int SizeBlock = 25;	
 	
 	public MainGame(int width, int height) {
+		//width = 1200
+		//height = 700
 		this.width = width;
 		this.height = height;
+		this.StateGame = "MainInterface";
+		this.NumMap = 1;
 	}
 	@Override
 	public void create () {
 		if(this.StateGame == "GameRunning") {
-			System.out.println("Hello in create");
-			ManageGameRunning = new GameRunning(this);
+			System.out.println("Hello in create in GameRunning");
+			this.ManageGameRunning = new GameRunning(this);
+		}
+		else if(this.StateGame == "MainInterface") {
+			System.out.println("Hello in create in Interface01");
+			this.ManageInterface = new MainInterface(this);
 		}
 	}
 
@@ -37,7 +46,9 @@ public class MainGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		if(this.StateGame == "GameRunning")
-			ManageGameRunning.Draw();
+			this.ManageGameRunning.Draw();
+		else if(this.StateGame == "MainInterface")
+			this.ManageInterface.Draw();
 //		DataMap.DrawBlock();
 //		System.out.println("Test int : "+this.Result);
 
---
+++ core/src/com/findthewaygame/mygdx/game/MainInterface.java
@@ -0,0 +1,62 @@
+package com.findthewaygame.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MainInterface {
+	public MainGame Main;
+	public String Status;
+	public SpriteBatch TextSprite = new SpriteBatch();
+	//public Texture TestSize36 = new Texture("Text_Test_Size36.png");
+	//public Texture TestSize40 = new Texture("Text_Test_Size40.png");
+	// Screen Width = 1200 Height = 700
+	// 1 cm = 28.4 pi
+	// width = 13.8 cm 
+	private Texture StartGame = new Texture("Text_StartGame_Size40.png");
+	private Texture ChooseMap = new Texture("Text_ChooseMap_Size40.png");
+	private Texture GreenCircle = new Texture("GreenCircle.png");
+	private int Point = 0;
+	
+	private float MiddleScreen;
+	
+	public MainInterface(MainGame Main) {
+		this.Main = Main;
+		this.Status = "MainInterface";
+		this.MiddleScreen = this.Main.width/2;
+	}
+	
+	public void Draw() {
+		this.Checkkey();
+		if(this.Status == "MainInterface") {
+			System.out.println("This is in draw MainInterface");
+			this.TextSprite.begin();
+			//this.TextSprite.draw(this.TestSize36,200,200);
+			//this.TextSprite.draw(this.TestSize40,200,160);
+			this.TextSprite.draw(this.StartGame, (float)(this.MiddleScreen - (13.8*14.2)), (float)(450));
+			this.TextSprite.draw(this.ChooseMap, (float)(this.MiddleScreen - (13.8*14.2)), (float)(250));
+			if(this.Point == 0)
+				this.TextSprite.draw(this.GreenCircle, (float)(this.MiddleScreen -(13*14.2)), (float)(465));
+			else
+				this.TextSprite.draw(this.GreenCircle, (float)(this.MiddleScreen - (13.7*14.2)), (float)(330));
+			this.TextSprite.end();
+		}
+	}
+	
+	private void Checkkey() {
+		if(this.Status == "MainInterface")
+			if(Gdx.input.isKeyJustPressed(Keys.UP)) 
+				this.Point = (this.Point - 1)%2;
+		
+			else if(Gdx.input.isKeyJustPressed(Keys.DOWN))
+				this.Point = (this.Point + 1)%2;
+		
+			else if(Gdx.input.isKeyPressed(Keys.RIGHT))
+				if(this.Point == 0) {
+					this.Main.NumMap = 1;
+					this.Main.StateGame = "GameRunning";
+					this.Main.create();
+				}
+	}
+}
